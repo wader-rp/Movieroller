cd4512ef7554d2e30ed12a5d6d16374c0a36f9e4
wader-rp <waddder@gmail.com>
2023-03-20 19:37:11 +0100

    styling

--- src/components/MovieResult/MovieResult.jsx
+++ src/components/MovieResult/MovieResult.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./movieResultStyles.css";
+import { Rate } from "antd";
 import { genres } from "../../data/MovieGenres";
 
 export const MovieResult = ({ randomMovie }) => {
@@ -10,20 +11,33 @@ export const MovieResult = ({ randomMovie }) => {
   console.log(genresNames);
 
   return (
-    <div>
-      <img
-        className="poster-image"
-        src={`https://image.tmdb.org/t/p/w200${randomMovie.poster_path}`}
-      />
-      <div className="title-year">
-        <div className="title-year title">{randomMovie.title} </div>
-        <div className="title-year year">
-          {`\u00A0(${randomMovie.release_date.substring(0, 4)})`}
+    <div className="movie-results-container">
+      <div className="movie-rating">
+        <Rate
+          disabled
+          allowHalf
+          count={10}
+          value={randomMovie.vote_average}
+          className="stars-rating"
+        />
+        <div>{`(${randomMovie.vote_count})`}</div>
+      </div>
+      <div className="movie-info">
+        <img
+          className="poster-image"
+          src={`https://image.tmdb.org/t/p/w200${randomMovie.poster_path}`}
+        />
+        <div className="title-year">
+          <div className="title-year title">{randomMovie.title} </div>
+          <div className="title-year year">
+            {`\u00A0(${randomMovie.release_date.substring(0, 4)})`}
+          </div>
+        </div>
+        <div className="overview">
+          <div className="overview-text">{randomMovie.overview}</div>
         </div>
       </div>
-      <div className="overview">
-        <div className="overview-text">{randomMovie.overview}</div>
-      </div>
+      <div className="streamings"></div>
     </div>
   );
 };
